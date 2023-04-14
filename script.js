a1a8d1306ad0f5c10721115fe4832adecf402a98
Seokhee Park <101683784+stoneHee99@users.noreply.github.com>
2023-04-14 10:17:42 +0900

    test webcam

--- script.js
+++ script.js
@@ -206,6 +206,16 @@ newTile.classList.add("selected");
   highlighted = index;
 }
 
-function showFanfare() {
-  alert("success!");
-}
+const video = document.getElementById('video');
+
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    video.srcObject = stream;
+                })
+                .catch(function (error) {
+                    console.error('Error accessing the webcam:', error);
+                });
+        } else {
+            console.error('getUserMedia not supported in this browser.');
+        }
